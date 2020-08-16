7643acd70f24fdf3104561f892e9d30e29a23e53
Rallisson <rallisson@outlook.com>
2020-08-16 09:20:39 -0400

    Limpeza de todo o código

--- Program.cs
+++ Program.cs
@@ -8,55 +8,7 @@ namespace OO_Course
     {
         static void Main(string[] args)
         {
-            string[] vet = Console.ReadLine().Split(' ');
-            int m = int.Parse(vet[0]);
-            int n = int.Parse(vet[1]);
-
-            int[,] matriz = new int[m, n]; 
-
-            for (int i = 0; i < m; i++)
-            {
-                vet = Console.ReadLine().Split(' ');
-
-                for (int j = 0; j < n; j++)
-                {
-                    matriz[i, j] = int.Parse(vet[j]);
-                }
-            }
-
-            int x = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (matriz[i, j] == x)
-                    {
-                        Console.WriteLine("Position " + i +", " + j + ":");
-                        
-                        if (i != 0)
-                        {
-                            Console.WriteLine("Up: " + matriz[i - 1, j]);
-                        }
-
-                        if(j != 0)
-                        {
-                            Console.WriteLine("Left: " + matriz[i, j - 1]);
-                        }
-                        
-                        if (j != n - 1)
-                        {
-                            Console.WriteLine("Right: " + matriz[i, j + 1]);
-                        }
-
-                        if (i != m - 1)
-                        {
-                            Console.WriteLine("Down: " + matriz[i + 1, j]);
-                        }
-                    }
-                }
-            }
-            Console.WriteLine("testando mudança no github");
+            
         }
     }
 }
