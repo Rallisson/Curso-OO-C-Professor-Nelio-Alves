0be11eb8983fcf4ec01f4060f599bf0805cde0a0
Rallisson <rallisson@outlook.com>
2020-08-16 18:53:42 -0400

    exercicio de enumeracoes e composicao

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Globalization;
 using System.Collections.Generic;
+using OO_Course.Entities;
+using OO_Course.Entities.Enums;
 
 namespace OO_Course
 {
@@ -8,7 +10,53 @@ namespace OO_Course
     {
         static void Main(string[] args)
         {
-            
+            // Client:
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+
+            // Order:
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            DateTime dateOrder = DateTime.Now;
+
+            Client client = new Client(name, email, date);
+            Order order = new Order(dateOrder, status, client);
+
+            // Product and OrderItem:
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                // Product:
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                Product product = new Product(productName, price);
+
+                // OrderItem:
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem item = new OrderItem(quantity, price, product);
+
+                // Order:
+                order.AddItem(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY");
+            Console.WriteLine(order);
+
         }
     }
 }
