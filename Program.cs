64d6e8e9a73553612fc24154934826105d8c68ab
Rallisson <rallisson@outlook.com>
2020-09-08 19:45:00 -0400

    limpeza do codigo do exercicio anterior

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using OO_Course.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 
@@ -9,48 +8,7 @@ namespace OO_Course
     {
         static void Main(string[] args)
         {
-            List<Product> list = new List<Product>();
-
-            Console.Write("Enter the number of products: ");
-            int n = int.Parse(Console.ReadLine());
-
-            for(int i = 1; i <= n; i++)
-            {
-                Console.WriteLine($"Product {i} data:");
-                Console.Write("Common, used or imported (c/u/i)? ");
-                string type = Console.ReadLine();
-
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-                Console.Write("Price: ");
-                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                if (type == "c")
-                {
-                    list.Add(new Product(name, price));
-                }
-
-                else if (type == "u")
-                {
-                    Console.Write("Manufacture date (DD/MM/YYYY): ");
-                    DateTime date = DateTime.Parse(Console.ReadLine());
-                    list.Add(new UsedProduct(name, price, date));
-                }
-
-                else if (type == "i")
-                {
-                    Console.Write("Customs fee: ");
-                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    list.Add(new ImportedProduct(name, price, customsFee));
-                }
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("PRICE TAGS:");
-            foreach(Product pro in list)
-            {
-                Console.WriteLine(pro.PriceTag());
-            }
+            
         }
     }
 }
